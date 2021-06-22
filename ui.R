87a38d4424c71732deb4bc9b3723d807218da1a9
wptmdoorn <wptmdoorn@gmail.com>
2021-06-22 12:31:07 +0000

    implement functionality

--- ui.R
+++ ui.R
@@ -1,16 +1,30 @@
 library(shiny)
+library(shinyjs)
 
-shinyUI(fluidPage(
-  
-  titlePanel("Histogram"),
-  
-  sidebarPanel(
-    sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
-    sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
-  ),
-  
-  mainPanel(
-    uiOutput("reacOut")
+ui <- shinyUI(
+  fluidPage(
+    shinyjs::useShinyjs(),
+    tags$script(
+      HTML(
+        'setInterval(function(){ $("#hiddenButton").click(); }, 1000*30);'
+      )
+    ),
+    tags$footer(shinyjs::hidden(
+      actionButton(inputId = "hiddenButton", label = "hidden")
+    )),
+    
+    titlePanel("Pie Chart"),
+    
+    sidebarPanel(
+      textInput("titlePlot", "Plot title", NULL),
+      textInput("titleLegend", "Legend title", NULL),
+      sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
+      sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
+      sliderInput("ringSize", "Inner ring size (R0)", 0, 1, 0.8),
+      downloadButton('downloadPlot', 'Download Plot')
+    ),
+    
+    mainPanel(uiOutput("reacOut"))
+    
   )
-  
-))
+)
--- workspace.R
+++ workspace.R
@@ -2,63 +2,153 @@ library(shiny)
 library(tercen)
 library(dplyr)
 library(tidyr)
+library(RColorBrewer)
+library(ggplot2)
+library(ggforce)
 
 ############################################
 #### This part should not be included in ui.R and server.R scripts
+# http://localhost:5402/admin/w/73f2fb84b22bb5542af0c50c9c14b930/ds/0ab4a97f-8852-414b-b5b9-020591e44a86
 getCtx <- function(session) {
-  ctx <- tercenCtx(stepId = "9b7619c7-4d66-49fa-9bb3-2b06209e58e4",
-                   workflowId = "f81d245ef22a2ff192ed2533a6002ec3")
+  ctx <- tercenCtx(stepId = "0ab4a97f-8852-414b-b5b9-020591e44a86",
+                   workflowId = "73f2fb84b22bb5542af0c50c9c14b930")
   return(ctx)
 }
 ####
 ############################################
 
 ui <- shinyUI(fluidPage(
-  
-  titlePanel("Histogram"),
+  titlePanel("Pie Chart"),
   
   sidebarPanel(
+    textInput("titlePlot", "Plot title", NULL),
+    textInput("titleLegend", "Legend title", NULL),
     sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
     sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
+    sliderInput("ringSize", "Inner ring size (R0)", 0, 1, 0.8),
+    downloadButton('downloadPlot', 'Download Plot')
   ),
   
-  mainPanel(
-    uiOutput("reacOut")
-  )
+  mainPanel(uiOutput("reacOut"))
   
 ))
 
 server <- shinyServer(function(input, output, session) {
-  
   dataInput <- reactive({
     getValues(session)
   })
   
   output$reacOut <- renderUI({
-    plotOutput(
-      "main.plot",
-      height = input$plotHeight,
-      width = input$plotWidth
-    )
-  }) 
+    plotOutput("main.plot",
+               height = input$plotHeight,
+               width = input$plotWidth)
+  })
   
   output$main.plot <- renderPlot({
-    values <- dataInput()
-    data <- values$data$.y
-    hist(data)
+    p()
   })
+  
+  p <- function() {
+    values <- dataInput()
+    
+    if (length(levels(values$data$label)) > 74) {
+      qual_col_pals = brewer.pal.info
+    } else {
+      qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual', ]
+    }
+    
+    col_vector = unlist(mapply(
+      brewer.pal,
+      qual_col_pals$maxcolors,
+      rownames(qual_col_pals)
+    ))
+    
+    data <- values$data %>%
+      mutate(
+        end = 2 * pi * cumsum(count) / sum(count),
+        start = lag(end, default = 0),
+        middle = 0.5 * (start + end),
+        hjust = ifelse(middle > pi, 1, 0),
+        vjust = ifelse(middle < pi / 2 |
+                         middle > 3 * pi / 2, 0, 1)
+      )
+    
+    plt = ggplot(data) +
+      geom_arc_bar(aes(
+        x0 = 0,
+        y0 = 0,
+        r0 = input$ringSize,
+        r = 1,
+        start = start,
+        end = end,
+        fill = label
+      )) +
+      geom_text(aes(
+        x = 1.05 * sin(middle),
+        y = 1.05 * cos(middle),
+        label = count,
+        hjust = hjust,
+        vjust = vjust
+      )) +
+      coord_fixed() +
+      scale_x_continuous(
+        limits = c(-1.5, 1.5),
+        # Adjust so labels are not cut off
+        name = "",
+        breaks = NULL,
+        labels = NULL
+      ) +
+      scale_y_continuous(
+        limits = c(-1.5, 1.5),
+        # Adjust so labels are not cut off
+        name = "",
+        breaks = NULL,
+        labels = NULL
+      ) +
+      scale_fill_manual(values = col_vector[1:length(unique(data$label))]) +
+      labs(fill = input$titleLegend,
+           title = input$titlePlot) +
+      theme(
+        panel.background = element_rect(fill = "transparent"),
+        # bg of the panel
+        plot.background = element_rect(fill = "transparent", color = NA),
+        # bg of the plot
+        panel.grid = element_blank(),
+        panel.grid.major = element_blank(),
+        # get rid of major grid
+        panel.grid.minor = element_blank(),
+        # get rid of minor grid
+        legend.background = element_rect(fill = "transparent"),
+        # get rid of legend bg
+        legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+      )
+    
+    plt
+    
+  }
+  
+  output$downloadPlot <- downloadHandler(
+    filename = 'pie.png',
+    content = function(file) {
+      ggsave(file,
+             plot = p(),
+             device = "png",
+             dpi = 300)
+    }
+  )
   
 })
 
-getValues <- function(session){
+getValues <- function(session) {
   ctx <- getCtx(session)
   values <- list()
-
-  values$data <- ctx %>% select(.y, .ri, .ci) %>%
-    group_by(.ci, .ri) %>%
-    summarise(.y = mean(.y)) # take the mean of multiple values per cell
-
+  
+  values$data <- ctx$select(c(ctx$labels[[1]], '.y')) %>%
+    setNames(c('label', 'count'))
+  
+  values$data <- as.data.frame(values$data)
+  
   return(values)
 }
 
-runApp(shinyApp(ui, server))  
+runApp(shinyApp(ui, server))
